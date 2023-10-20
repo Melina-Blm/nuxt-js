c2b9ee0d9c9dfb2b35f2bd19f39bad3c6444986e
Mihoko <melina.bouchelleghem@edu.univ-fcomte.fr>
2023-10-20 22:57:09 +0200

    feat(page404) : création d'un customtype page404 + connexion document à nuxt

--- error.vue
+++ error.vue
@@ -1,5 +1,12 @@
 <script setup>
+const { client } = usePrismic()
+const { data: errorPage, error } = await useAsyncData('errorPage', () => client.getSingle('page404'))
 
+// Handle case when request fails
+if (!errorPage.value || error.value) {
+  throw createError({ statusCode: 404, statusMessage: 'La page d\'erreur 404 est introuvable' })
+}
+const env = useRuntimeConfig()
 const props = defineProps ({
     error: Object
 })
@@ -14,10 +21,14 @@ const props = defineProps ({
 
     <div class="p-error">
  {{ error }}
-
+ <p>Oops! Something went wrong.</p>
+ <pre>
+ {{ errorPage.data.title[0].text}}
+ {{ errorPage.data.text[0].text}}
+</pre>
 
     </div>
-    
+    <!-- {{ page404.data }} -->
 </div>
 <MyFooter/>
 </template>
