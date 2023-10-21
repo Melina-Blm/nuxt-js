c85f6e35841692afc53879d610744a0e2e9c6e2e
Mihoko <melina.bouchelleghem@edu.univ-fcomte.fr>
2023-10-21 12:30:42 +0200

    feat(page404) : mise en forme scss et contenu prismic

--- error.vue
+++ error.vue
@@ -15,28 +15,60 @@ const props = defineProps ({
 
 <template>
     <div class="content">
+        <MyHeader/>
+        <div class="content-leftside">
+           <BikeDelivery/>
+           <PrismicRichText class="content-leftside__title" :field="errorPage.data.title" />
+           <PrismicRichText class="content-leftside__text" :field="errorPage.data.text" />
+           <div class="btn-more">
+            <MyButton variant="rounded" :hasIcon="true" size="small"><router-link to="/">Return home</router-link></MyButton>
+            </div>
 
-    
-    <MyHeader/>
+        </div>
 
-    <div class="p-error">
- {{ error }}
+  
+ <!-- {{ error }}
  <p>Oops! Something went wrong.</p>
  <pre>
  {{ errorPage.data.title[0].text}}
  {{ errorPage.data.text[0].text}}
-</pre>
+</pre> -->
+
 
     </div>
-    <!-- {{ page404.data }} -->
-</div>
+
 <MyFooter/>
 </template>
 
 <style lang="scss" scoped>
 
 .content {
-    margin:rem(0) rem(80)
-}
+    margin:rem(0) rem(80);
+    &-leftside {
 
+        &__title {
+        font-size: $giant-font-size;
+        font-weight: 700;
+        line-height: 1.2;
+        color: black;
+        margin: rem(30) rem(0);
+
+    }
+    &__text {
+        font-size: $medium-font-size;
+        font-weight: 400;
+        line-height: 1.5;
+        color: $primary-color;
+        margin: rem(30) rem(0);
+    }
+    
+    }
+    }
+
+
+
+  .btn-more a{
+  text-decoration: none;
+  color:$white;
+  }
 </style>
