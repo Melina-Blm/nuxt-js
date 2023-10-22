fe92d0251f3aded04e4e5293e035ec32e8034132
Mihoko <melina.bouchelleghem@edu.univ-fcomte.fr>
2023-10-22 09:56:17 +0200

    fix(clean) optimisation du code (nommage classe ABEM + suppression stories)

--- components/Information/Information.vue
+++ components/Information/Information.vue
@@ -6,16 +6,15 @@ const props = defineProps({
 
 <template>
   <section class="c-information">
-    <div v-for="item in information" class="c-information__item -secondBorder">
-      <!-- Matching prismic options / file names -->
-      <div class="c-information__icon">
+    <div v-for="item in information" class="c-information__item c-information__item--secondBorder">
+      <div class="c-information__item__icon">
         <img :src="`/icons/${item.information_icon}.svg`" alt="">
       </div>
 
-      <div class="c-information__title">
+      <div class="c-information__item__title">
         <PrismicRichText :field="item.information_title" />
       </div>
-      <div class="c-information__text">
+      <div class="c-information__item__text">
         <PrismicRichText :field="item.information_text" />
       </div>
     </div>
@@ -33,7 +32,8 @@ const props = defineProps({
   background: $white;
   box-shadow: 0px 0px 50px 0px rgba(0, 0, 0, 0.11);
   padding: rem(25);
-    &__item {
+
+  &__item {
     display: flex;
     justify-content: center;
     align-items: center;
@@ -41,52 +41,53 @@ const props = defineProps({
     flex: 1;
     gap: rem(7);
     height: rem(163);
-    &:nth-child(2n){
-    position: relative;
-    &::after,
-    &::before {
-      content: '';
-      background-color: $gray;
-      width: 1px;
-      display: block;
-      height: 100%;
-      position: absolute;
+
+    &:nth-child(2n) {
+      position: relative;
+
+      &::after,
+      &::before {
+        content: '';
+        background-color: $gray;
+        width: 1px;
+        display: block;
+        height: 100%;
+        position: absolute;
+      }
+
+      &::after {
+        left: 0;
+      }
+
+      &::before {
+        right: 0;
+      }
     }
 
-    &::after {
-      left: 0;
+    &__icon {
+      display: inline-flex;
+      width: 30px;
+      height: 30px;
+      padding: 10px;
+      align-items: center;
+      justify-content: center;
+      background-color: $primary-color;
+      border-radius: 50%;
     }
 
-    &::before {
-      right: 0;
+    &__title {
+      font-weight: 700;
+
+      &:not(:first-child) {
+        margin-top: 10px;
+      }
     }
-  
+
+    &__text {
+      &:not(:first-child) {
+        margin-top: 10px;
+      }
+    }
+  }
 }
-  }
-  &__icon {
-    display: inline-flex;
-    width: 30px;
-    height: 30px;
-    padding: 10px;
-    align-items: center;
-    justify-content: center;
-    background-color: $primary-color;
-    border-radius: 50%;
-  }
-
-  &__title {
-    font-weight: 700;
-
-    &:not(:first-child) {
-      margin-top: 10px;
-    }
-  }
-
-  &__text {
-    &:not(:first-child) {
-      margin-top: 10px;
-    }
-  }
-}
-
-  </style>
+</style>
