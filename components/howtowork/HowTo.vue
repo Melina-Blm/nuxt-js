6e3ed3eafbd6f4935f2d630f72bdc55490c1b631
Mihoko <melina.bouchelleghem@edu.univ-fcomte.fr>
2023-11-17 13:53:42 +0100

    fix(howto) : fix des images line 1 & 2

--- components/howtowork/HowTo.vue
+++ components/howtowork/HowTo.vue
@@ -16,8 +16,8 @@ const isOdd = (index) => {
 
 <template>
     <section class="c-how-to">
-      <span class="c-how-to__text">{{ tag }}</span>
-      <h2 class="c-how-to__title">{{ title }}</h2>
+        <span class="c-how-to__text">{{ tag }}</span>
+        <h2 class="c-how-to__title">{{ title }}</h2>
         <div class="c-how-to-content">
             <div v-for="(item, index) in items" :key="index">
                 <HowToItem :reversed="isOdd(index)" :middle="index === 1" :end="index === 2" :endtext="index === 2"
@@ -38,56 +38,64 @@ const isOdd = (index) => {
 <style lang="scss" scoped>
 .c-how-to {
     position: relative;
+
     &__text {
         display: flex;
-    justify-content: center;
-    color:$primary-color;
-    margin-top: rem(130);
-    font-size: $medium-font-size;
-    position: relative  ;
+        justify-content: center;
+        color: $primary-color;
+        margin-top: rem(130);
+        font-size: $medium-font-size;
+        position: relative;
 
     }
+
     &__title {
         display: flex;
-    justify-content: center;
-    font-size:$big-font-size;
-    margin-top:rem(10);
+        justify-content: center;
+        font-size: $big-font-size;
+        margin-top: rem(10);
     }
+
     &-content {
         display: flex;
         justify-content: space-between;
         margin: rem(70) rem(0);
 
-        &-line,
-        &-line2 {
-            display: none;
-        }
+
     }
 
-}    
+    &-line,
+    &-line2 {
+        display: none;
+    }
+}
 
 @media screen and (min-width:1510px) and (max-width:1537px) {
     .c-how-to {
+        
             &-line {
-              img{
+
                 display: block;
                 position: absolute;
-                width: 26vw;
+                
                 top: 38.5%;
                 left: 10.5%;
-              }
+                img{
+                    width: 26vw;
+                }
             }
 
             &-line2 {
-              img{
+
                 display: block;
                 position: absolute;
-                width: 26vw;
                 top: 38.5%;
                 right: 10.5%;
-              }
+                img{
+                    width: 26vw;
+                }
             }
-        }
-    
-}
-</style>
+        
+    }
+
+}</style>
